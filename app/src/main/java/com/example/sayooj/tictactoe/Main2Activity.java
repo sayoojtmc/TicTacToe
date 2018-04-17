84e5d4d77dec264feebea8d608bd38aa35fbc6af
sayoojtmc <sayoojtmc@gmail.com>
2018-04-17 12:04:32 +0530

    Start menu :)

---
+++ app/src/main/java/com/example/sayooj/tictactoe/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.sayooj.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button start=findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity1();
+
+            }
+        });}
+
+        public void openactivity1() {
+            Intent i=new Intent(this,MainActivity.class);
+            startActivity(i);
+        }
+    }
+
